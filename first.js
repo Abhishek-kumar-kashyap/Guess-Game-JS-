1d7ffbd3777524228555c483ec31e31cb60c94a2
Abhishek-kumar-kashyap <162340892+Abhishek-kumar-kashyap@users.noreply.github.com>
2024-03-19 23:55:56 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,24 @@
+const max = prompt("Enter a maximum number");
+
+const random = Math.floor(Math.random() * max) + 1;
+console.log(random);
+
+let guess = prompt("Guess the Number!");
+
+while(true){
+    if(guess == "quit"){
+        console.log("you are quit!")
+        break;
+    }
+
+    if(guess == random){
+        console.log("You are Right! Congrats!! randon number was", random)
+        break;
+    } else if(guess < random){
+       guess = prompt("Hint : Your guess was too small. Please try higher.")
+    }
+    else{
+        guess = prompt("Hint : Your guess was too large. Please try smaller")
+    }
+
+}
